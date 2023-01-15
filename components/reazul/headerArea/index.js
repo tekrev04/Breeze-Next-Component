635718409331d2be4892187d3b23158d48bfc953
reazul7 <islamreazul7777@gmail.com>
2023-01-15 14:53:48 +0600

    dynamic menu-button components update

--- components/reazul/headerArea/index.js
+++ components/reazul/headerArea/index.js
@@ -1,26 +1,53 @@
 import React from "react";
+import ButtonComponent from "../elements/buttonComponent";
 import HeaderAreaLeftComponent from "./headerAreaLeftComponent";
 import HeaderAreaRightComponent from "./headerAreaRightComponent";
+import { BsStar } from "react-icons/bs";
+import { RiDraftLine } from "react-icons/ri";
+import { SiNike } from "react-icons/si";
+import { MdOutlineDashboard } from "react-icons/md";
+import MenuHeaderComponent from "../menuHeader";
 
 function HeaderAreaComponent() {
-    return (
-        <>
-            <div className="header-main-area shadow text-dark flex items-center justify-between px-3 py-1">
-                <h4>Teknovus Limited</h4>
-                <HeaderAreaRightComponent />
-            </div>
+  const onClick = () => {
+    console.log("clicked btn");
+  };
 
-            <div className="header-main-area shadow bg-dark-light text-dark flex items-center justify-between px-2 pt-4 pb-3">
-                <HeaderAreaLeftComponent headerTitle={"Order Management"} />
-            </div>
-        </>
+  const menuBtnInfo = [
+    {
+      name: "All",
+      icon: <MdOutlineDashboard />,
+    },
+    {
+      name: "Follow-up",
+      icon: <BsStar />,
+    },
+    {
+      name: "Draft",
+      icon: <RiDraftLine />,
+    },
+    {
+      name: "Sewing Cut",
+      icon: <SiNike />,
+    },
+  ];
 
-        // <div className="header-main-area bg-dark-light text-dark flex justify-between">
-        //     <HeaderAreaLeftComponent headerTitle={"Order Management"} />
+  return (
+    <>
+      <div className="header-main-area shadow text-dark flex items-center justify-between px-3 py-1">
+        <h4>Teknovus Limited</h4>
+        <HeaderAreaRightComponent />
+      </div>
 
-        //     <HeaderAreaRightComponent />
-        // </div>
-    );
+      <div className="header-main-area shadow bg-dark-light text-dark flex items-center justify-between px-2 pt-4 pb-3">
+        <HeaderAreaLeftComponent headerTitle={"Order Management"} />
+      </div>
+
+      <div>
+        <MenuHeaderComponent menuBtnInfo={menuBtnInfo} />
+      </div>
+    </>
+  );
 }
 
 export default HeaderAreaComponent;
---
+++ components/reazul/menuHeader/index.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const MenuHeaderComponent = (props) => {
+  const { menuBtnInfo } = props;
+  const [activeButton, setActiveButton] = useState(0);
+
+  return (
+    <div className="button-list">
+      {menuBtnInfo.map((buttonInfo, index) => (
+        <button
+          key={index}
+          className={`btn ${activeButton === index ? "menu-btn-active" : "menu-btn"}`}
+          onClick={() => setActiveButton(index)}
+        >
+          <div className="flex">
+            <span className="pt-1 pr-2">{buttonInfo.icon}</span>
+            {buttonInfo.name}
+          </div>
+        </button>
+      ))}
+    </div>
+  );
+};
+export default MenuHeaderComponent;
